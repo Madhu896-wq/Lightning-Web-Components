100882006e7f2bdba24074c414d79d470bdcaeb6
Madhu <mp0036092@gmail.com>
2025-10-04 18:45:40 +0530

    Change

--- force-app/main/default/lwc/recordApplication/recordApplication.js
+++ force-app/main/default/lwc/recordApplication/recordApplication.js
@@ -1,5 +1,5 @@
 import { LightningElement } from 'lwc';
 
 export default class RecordApplication extends LightningElement {
-    fields = ['Candidate_Name__c','Email__c','Position_Applied_For__c','Status__c','Phone_Number__c	'];
+    fields = ['Candidate_Name__c','Email__c','Position_Applied_For__c','Status__c','Phone_Number__c','Amount'];
 }
